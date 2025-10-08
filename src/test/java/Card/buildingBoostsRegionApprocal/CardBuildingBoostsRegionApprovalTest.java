a7d4fc790dd72e2aee38b8a811aed506ecf55944
tine-debug <134289819+Tine-debug@users.noreply.github.com>
2025-10-08 23:27:37 +0200

    added: buildingBootsRegionApprocal

---
+++ src/test/java/Card/buildingBoostsRegionApprocal/CardBuildingBoostsRegionApprovalTest.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.approvaltests.Approvals;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class CardBuildingBoostsRegionApprovalTest{
+
+    static String[] buildingNames = {"Iron Foundry", "Grain Mill", "Lumber Camp",  "Brick Factory", "Weaver’s Shop", "Weaver's Shop", "x", null};
+    static String[] regionNames = {"Mountain","Field", "Forest", "Hill", "Pasture", "x", null};
+
+
+    static Stream<Arguments> RegionandBuldingNamesProvider() {
+    List<Arguments> args = new ArrayList<>();
+    for (int i = 0; i < buildingNames.length ; i++) {
+        for (int j = 0; j < regionNames.length; j++)
+            args.add(Arguments.of( buildingNames[i], regionNames[j]));    
+    }
+    return args.stream();
+    }
+
+
+        @ParameterizedTest(name = "[{index}]-{0},{1}buildingBoostsRegion")
+        @MethodSource("RegionandBuldingNamesProvider")
+        void testCardbuildingBoostsRegion (String buildingname, String regionname) {
+            String result = String.valueOf(Card.buildingBoostsRegion(buildingname, regionname));
+            Approvals.verify(result, Approvals.NAMES.withParameters(buildingname, regionname));
+        }
+}
