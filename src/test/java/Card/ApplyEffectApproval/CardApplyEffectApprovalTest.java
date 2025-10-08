3cb5e882e98ff8a75846a769867f567503f3b5b0
tine-debug <134289819+Tine-debug@users.noreply.github.com>
2025-10-08 13:44:05 +0200

    feat: remodeled card apply effect test + approved files

--- src/test/java/Card/ApplyEffectApproval/CardApplyEffectApprovalTest.java
+++ src/test/java/Card/ApplyEffectApproval/CardApplyEffectApprovalTest.java
@@ -5,7 +5,6 @@ import java.util.stream.Stream;
 
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -26,22 +25,32 @@ public class CardApplyEffectApprovalTest {
     static Vector<Card> basicCards;
     StubPlayer player1;
     StubPlayer player2;
-    static int[][] interestingFields = {{1,0},{1,1},{1,2},{2,1},{2,2},{3,2},{2,0}};
+    static Vector<int[]> interestingFields = new Vector<>();
 
 
     @BeforeAll
-    static void setupall() {
+    static void setupCardsandFields() {
         try {
-            basicCards = Card.loadThemeCards("cards.json", "basic");
+            basicCards = Card.loadThemeCards("cards.json", "basic", false);
             Card.loadBasicCards("cards.json");
         } catch (Exception e) {
-        }
             System.err.println("Failed to load");
         }
+            for (int i = 0; i<5 ; i++){
+                for (int j = 0; j<6 ; j++){
+                    interestingFields.add(new int[]{i, j});
+                }
+            }
+             interestingFields.add(new int[]{0,1});
+             interestingFields.add(new int[]{0,3});
 
-    @BeforeEach
-    void setupeach() {
-        try {
+
+        }
+
+
+
+    void setupPlayer1(){
+                try {
             Card.loadBasicCards("cards.json");
         } catch (Exception e) {
             System.err.println("Failed to load");
@@ -56,55 +65,102 @@ public class CardApplyEffectApprovalTest {
         player1.gainResource("Wool");
         int[][] regionDice = { { 2, 1, 6, 3, 4, 5 }, { 3, 4, 5, 2, 1, 6 } };
         Server.pricipalityinitoneplayer(player1, regionDice, 2,  1);
+
+    }
+
+     void setupPlayer2 (){
         try {
-            Card.loadBasicCards("cards.json");
-        } catch (Exception e) {
-            System.err.println("Failed to load");
-        }
+                    Card.loadBasicCards("cards.json");
+                } catch (Exception e) {
+                    System.err.println("Failed to load");
+                }
+                player2 = new StubPlayer();
+                int[][] regionDice = { { 2, 1, 6, 3, 4, 5 }, { 3, 4, 5, 2, 1, 6 } };
+                Server.pricipalityinitoneplayer(player2, regionDice, 2,  1);
+
+
     }
 
 
-    /*@ParameterizedTest
-    @
-    void applyEffectPlayer2Player2Test(){
-        for (int i = 0; i < basicCards.size(); i++){
-            player2 = new Player();
-            int[][] regionDice = { { 2, 1, 6, 3, 4, 5 }, { 3, 4, 5, 2, 1, 6 } };
-            Server.pricipalityinitoneplayer(player2, regionDice, 2,  1);
-            for (int j = 0; j < 6; j++){
-                String result = String.valueOf(basicCards.get(i).applyEffect(player2, player2, interestingFields[j][0], interestingFields[j][1]));
-                Approvals.verify(
-                     result);
-            }
 
-        }
-        }
-
-        */
-
-
-    static Stream<Arguments> cardTestProvider() {
+    static Stream<Arguments> cardTestProviderwithindex() {
     List<Arguments> args = new ArrayList<>();
     for (int i = 0; i < basicCards.size(); i++) {
         Card card = basicCards.get(i);
-        for (int j = 0; j < interestingFields.length; j++) {
-            args.add(Arguments.of( card.name, i, j, card));
-        }
+            args.add(Arguments.of( card.name, i, card));
     }
     return args.stream();
     }
 
-        @ParameterizedTest(name = "[{index}]-{0}_at_{1}-{2}")
-        @MethodSource("cardTestProvider")
-        void testCardApplyEffectoneatIndividuallyp2p2 (String cardname,int i, int j, Card card) {
-            player2 = new StubPlayer();
-            int[][] regionDice = { { 2, 1, 6, 3, 4, 5 }, { 3, 4, 5, 2, 1, 6 } };
-            Server.pricipalityinitoneplayer(player2, regionDice, 2,  1);
-            boolean effectResult = card.applyEffect(player2, player2, interestingFields[j][0], interestingFields[j][1]);
-            String result = String.valueOf(effectResult);
-            result = result + "\n" + player2.printPrincipality();
-            Approvals.verify(result, Approvals.NAMES.withParameters(cardname + i + "at" + interestingFields[j][0] + "," + interestingFields[j][1]));
+
+        @ParameterizedTest(name = "[{index}]-p2p2{0}_individual")
+        @MethodSource("cardTestProviderwithindex")
+        void testCardApplyEffectoneatIndividuallyp2p2 (String cardname,int i, Card card) {
+            String result = "";
+            setupPlayer2();
+            for (int j = 0; j < interestingFields.size(); j++)
+            {
+                result = result + "\n at: " + interestingFields.get(j)[0] + "," + interestingFields.get(j)[1] + "\n";
+                boolean effectResult = card.applyEffect(player2, player2, interestingFields.get(j)[0], interestingFields.get(j)[1]);
+                result = result + String.valueOf(effectResult);
+                result = result + "\n" + player2.printPrincipality();
+                result = result + "\n" + player2.flags.toString();
+            }
+            Approvals.verify(result, Approvals.NAMES.withParameters(i + cardname));
         }
+
+        @ParameterizedTest(name = "[{index}]-p2p1{0}_individual")
+        @MethodSource("cardTestProviderwithindex")
+        void testCardApplyEffectoneatIndividuallyp2p1 (String cardname,int i, Card card) {
+            String result = "";
+            setupPlayer1();
+            setupPlayer2();
+            for (int j = 0; j < interestingFields.size(); j++)
+            {
+                result = result + "\n at: " + interestingFields.get(j)[0] + "," + interestingFields.get(j)[1] +  "\n";
+                boolean effectResult = card.applyEffect(player2, player1, interestingFields.get(j)[0], interestingFields.get(j)[1]);
+                result = result + String.valueOf(effectResult);
+                result = result + "\n" + player1.printPrincipality() + "\n" + player2.printPrincipality();
+                result = result + "\n" + player1.flags.toString() + "\n" + player2.flags.toString();
+            }
+            Approvals.verify(result, Approvals.NAMES.withParameters(i + cardname));
+        }
+
+
+        @ParameterizedTest(name = "[{index}]-p1p2{0}_individual")
+        @MethodSource("cardTestProviderwithindex")
+        void testCardApplyEffectp1p2 (String cardname,int i, Card card) {
+            String result = "";
+            setupPlayer1();
+            setupPlayer2();
+            for (int j = 0; j < interestingFields.size(); j++)
+            {
+                result = result + "\n at: " + interestingFields.get(j)[0] + "," + interestingFields.get(j)[1] +  "\n";
+                boolean effectResult = card.applyEffect(player1, player2, interestingFields.get(j)[0], interestingFields.get(j)[1]);
+                result = result + String.valueOf(effectResult);
+                result = result + "\n" + player1.printPrincipality() + "\n" + player2.printPrincipality();
+                result = result + "\n" + player1.flags.toString() + "\n" + player2.flags.toString();
+            }
+            Approvals.verify(result, Approvals.NAMES.withParameters(i + cardname));
+        }
+
+
+       @ParameterizedTest(name = "[{index}]-p1p1{0}_individual")
+        @MethodSource("cardTestProviderwithindex")
+        void testCardApplyEffectp1p1 (String cardname,int i, Card card) {
+            String result = "";
+            setupPlayer1();
+            for (int j = 0; j < interestingFields.size(); j++)
+            {
+                result = result + "\n at: " + interestingFields.get(j)[0] + "," + interestingFields.get(j)[1] +  "\n";
+                boolean effectResult = card.applyEffect(player1, player1, interestingFields.get(j)[0], interestingFields.get(j)[1]);
+                result = result + String.valueOf(effectResult);
+                result = result + "\n" + player1.printPrincipality();
+                result = result + "\n" + player1.flags.toString();
+            }
+            Approvals.verify(result, Approvals.NAMES.withParameters(i + cardname));
+        }
+
 
     
 
