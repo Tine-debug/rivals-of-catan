05c1db3f37dab1bc9e7348e05e62fd1795a3f918
tine-debug <134289819+Tine-debug@users.noreply.github.com>
2025-10-10 11:48:41 +0200

    ref: added ActionPlacement class

--- src/main/java/placement/PlacementFactory.java
+++ src/main/java/placement/PlacementFactory.java
@@ -3,6 +3,7 @@ public class PlacementFactory{
         if (placement.equals("Center Card")) return new CenterCardPlacement();
         if(placement.equals("Region")) return new RegionPlacement();
         if (placement.equals("Settlement/city")) return new SettlementExpansionPlacement();
+        if (placement.equals("Action")) return new ActionPlacement();
         return new Placement(placement);
     }
 
