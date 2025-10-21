299ab4210ce2e0cb32ce201ef94305d1d8b2211d
tine-debug <134289819+Tine-debug@users.noreply.github.com>
2025-10-21 13:06:47 +0200

    fix: Changed if in Logicfactory to handle single null values

--- src/main/java/Card/logic/LogicFactory.java
+++ src/main/java/Card/logic/LogicFactory.java
@@ -6,39 +6,38 @@ public class LogicFactory {
 
     public static Logic createLogic(String placement, String name, String type) {
 
-        if (name == null) {
-            return new defaultLogic();
-        }
-        if (name.equals("City")) {
-            return new CityLogic();
-        }
-        if (name.equals("Settlement")) {
-            return new SettlementLogic();
-        }
-        if (name.equals("Road")) {
-            return new RoadLogic();
-        }
-        if (name.equals("Merchant Caravan")) {
-            return new MerchantCaravanLogic();
-        }
-        if (name.equals("Relocation")) {
-            return new RelocationLogic();
-        }
+        if (name != null) {
 
-        if (type == null) {
-            return new defaultLogic();
+            if (name.equals("City")) {
+                return new CityLogic();
+            }
+            if (name.equals("Settlement")) {
+                return new SettlementLogic();
+            }
+            if (name.equals("Road")) {
+                return new RoadLogic();
+            }
+            if (name.equals("Merchant Caravan")) {
+                return new MerchantCaravanLogic();
+            }
+            if (name.equals("Relocation")) {
+                return new RelocationLogic();
+            }
         }
-        if (type.equals("Region")) {
-            return new RegionLogic();
+        if (type != null) {
+
+            if (type.equals("Region")) {
+                return new RegionLogic();
+            }
         }
-        if (placement == null) {
-            return new defaultLogic();
-        }
-        if (placement.equals("Settlement/city")) {
-            return new SettlementExpansionLogic();
-        }
-        if (placement.equals("Action")) {
-            return new ActionLogic();
+        if (placement != null) {
+
+            if (placement.equals("Settlement/city")) {
+                return new SettlementExpansionLogic();
+            }
+            if (placement.equals("Action")) {
+                return new ActionLogic();
+            }
         }
 
         return new defaultLogic();
